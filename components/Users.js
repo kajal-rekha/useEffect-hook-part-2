440ef7045449d2c08fc74feb49d384a73b137c07
kajal-2011 <kajalrekha888@gmail.com>
2022-11-23 21:14:25 +0600

    first commit

---
+++ components/Users.js
@@ -0,0 +1,64 @@
+import { useEffect, useState } from "react";
+
+const Users = () => {
+  const [users, setUsers] = useState([]);
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+
+  ///////////////////////////////////////////////////////////////
+  // const fetchUsers = () => {
+  //   fetch(`https://jsonplaceholder.typicode.com/users`)
+  //     .then((response) => {
+  //       if (!response.ok) throw new Error("Something went wrong");
+  //       return response.json();
+  //     })
+  //     .then((data) => {
+  //       setUsers(data);
+  //       setIsLoading(false);
+  //     })
+  //     .catch((error) => {
+  //       setError(error.message);
+  //       setIsLoading(false);
+  //     });
+  // };
+
+  /////////////////////////////////////////////////////////////////////
+  const fetchUsersTwo = async () => {
+    try {
+      const response = await fetch(
+        `https://jsonplaceholder.typicode.com/users`
+      );
+      if (!response.ok) throw new Error("Something went wrong");
+      const data = await response.json();
+      setUsers(data);
+      setIsLoading(false);
+    } catch (error) {
+      setError(error.message);
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    // fetchUsers();
+    fetchUsersTwo();
+  }, []);
+
+  return (
+    <div>
+      {isLoading ? <p>Loading...</p> : null}
+      {error ? (
+        <p>{error}</p>
+      ) : (
+        users.map((user) => (
+          <div key={user.id}>
+            <h2>{user.name}</h2>
+            <p>{user.email}</p>
+            <p>{user.phone}</p>
+          </div>
+        ))
+      )}
+    </div>
+  );
+};
+
+export default Users;
